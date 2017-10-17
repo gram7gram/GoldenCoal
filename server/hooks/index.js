24573e44ec636aed9922654e466f4f453d96626f
hesoy <s.shmygol@gmail.com>
2017-10-17 16:48:51 +0300

    Add slack integration #master in progress

---
+++ server/hooks/index.js
@@ -0,0 +1,18 @@
+const $ = require('jquery')
+const tokens = reuire('../config/parameters').tokens
+
+let xhr
+const hook = text => {
+	
+	xhr && xhr.abort()
+    xhr = $.ajax({
+        method: 'POST',
+        url: 'https://hooks.slack.com/services' + tokens.slackToken,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            text
+        })
+    })
+}
+
+module.exports = hook
